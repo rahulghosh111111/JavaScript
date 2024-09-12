a03b23cc641739f69a267b291dfd72a91daad15a
Rahul Ghosh <154549822+rahulghosh111111@users.noreply.github.com>
2024-09-12 10:11:25 +0000

    conversion of datatype

---
+++ 1_basics/conversionOperation.js
@@ -0,0 +1,21 @@
+// let score = "33"
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+
+//"33"=33
+//"33abd"=NaN
+//true=1
+//false=0
+
+let isLoggedIn ="2"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+let booleanIsLoggedIn = String(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+//1=>true; 0=>false
+//""=>false
+// "rahul" =>true
