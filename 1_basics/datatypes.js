ac990e4aae49180b55b24d770dfa02741b5782b1
Rahul Ghosh <154549822+rahulghosh111111@users.noreply.github.com>
2024-09-12 20:41:45 +0000

    datatypes

--- 1_basics/datatypes.js
+++ 1_basics/datatypes.js
@@ -1,25 +1,47 @@
-"use strict" // treat all js code as newer versio
+// "use strict" // treat all js code as newer versio
 
-// alart(2+9)  // we are using node js not browsing
+// // alart(2+9)  // we are using node js not browsing
 
-console.log(3+3);
-console.log("Rahul");
-
-
-let name = "rahul"
-let age = 22
-let isLoggedin = false
-
-//bigint
-//string
-//boolean
-// null = standalone value   (represent of empty value)
-//undefined
-//symbol => unique
+// // console.log(3+3);
+// // console.log("Rahul");
 
 
-//object
+// let name = "rahul"
+// let age = 22
+// let isLoggedin = false
 
-console.log(typeof "Rahul");  // what type of this datatype
-console.log(typeof null);
+// //bigint
+// //string
+// //boolean
+// // null = standalone value   (represent of empty value)
+// //undefined
+// //symbol => unique
 
+
+// //object
+
+// // console.log(typeof "Rahul");  // what type of this datatype
+// // console.log(typeof null);
+
+
+
+
+// //***************************Summary****************************/
+
+// // premitive
+// // 7 types : String , Number , Boolean , null , undefined , Symbol , BigInt
+
+
+// const score = 100
+// const scoreValue=100.3
+
+
+// const isloggedin= false
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+// //Reference (Non premitive)
+// // Arrays , Objects , Functions
