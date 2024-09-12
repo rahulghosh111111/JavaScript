1f29f2d4a06f06d038eaf2e1b7f30aeb54595d91
Rahul Ghosh <154549822+rahulghosh111111@users.noreply.github.com>
2024-09-12 20:26:52 +0000

    comparision

---
+++ 1_basics/comparision.js
@@ -0,0 +1,13 @@
+// console.log("Rahul")
+// console.log(2>1)
+
+
+// console.log(null>0)
+// console.log(null==0)
+// console.log(null>=0)
+
+// console.log(undefined == 0)  //always value is false
+
+
+console.log("2"===2);
+console.log(2===2);
