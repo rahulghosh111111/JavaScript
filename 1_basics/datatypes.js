901bceb047b9e9d73c8bacc2d89c2374aed6af26
Rahul Ghosh <154549822+rahulghosh111111@users.noreply.github.com>
2024-09-12 08:31:51 +0000

    datatypes

---
+++ 1_basics/datatypes.js
@@ -0,0 +1,25 @@
+"use strict" // treat all js code as newer versio
+
+// alart(2+9)  // we are using node js not browsing
+
+console.log(3+3);
+console.log("Rahul");
+
+
+let name = "rahul"
+let age = 22
+let isLoggedin = false
+
+//bigint
+//string
+//boolean
+// null = standalone value   (represent of empty value)
+//undefined
+//symbol => unique
+
+
+//object
+
+console.log(typeof "Rahul");  // what type of this datatype
+console.log(typeof null);
+
---
+++ 1_basics/variables.js
@@ -0,0 +1,7 @@
+const accountId = 2202222 // once you assign then cannot be change in const variable
+let accEmail = "rahulg@gmail.com"
+var accPass = "22334"
+accountCity = "bbs"
+
+console.log(accountId);// print one time
+console.table([accEmail,accPass,accountCity,accountId]) // print all data in table form
