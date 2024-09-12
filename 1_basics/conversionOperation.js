c196c7994c1730dd5a375dacd47b01e0c8e9ea7f
Rahul Ghosh <154549822+rahulghosh111111@users.noreply.github.com>
2024-09-12 10:30:28 +0000

    conversion of datatype

--- 1_basics/conversionOperation.js
+++ 1_basics/conversionOperation.js
@@ -14,8 +14,49 @@ let isLoggedIn ="2"
 // let booleanIsLoggedIn = Boolean(isLoggedIn)
 let booleanIsLoggedIn = String(isLoggedIn)
 
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 //1=>true; 0=>false
 //""=>false
 // "rahul" =>true
+
+//**************************OPERATIONS********************************/
+
+
+let value = 2
+let negValue = -value
+// console.log(negValue);
+
+
+// console.log(2+3);
+// console.log(2-2); // all  are same
+
+
+
+let str1 = "hello"
+let str2 = " Rahul"
+
+let str3 = str1+str2
+// console.log(str3);
+
+
+
+// console.log("1" +2);
+// console.log(1 +"2");
+// console.log("1" +2+2);  //system take the first operation for all like string
+// console.log(1+2+"9");
+
+
+// console.log(+true);  //boolean 
+// console.log(+"");
+
+
+let num1, num2,num3
+
+nym1=num2=num3=2+2
+
+
+let gameCounter = 100
+// gameCounter++   //prefix and postfix
+++gameCounter  
+console.log(gameCounter);
