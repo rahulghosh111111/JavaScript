61a57cfeda3f172323a9b03e179abef10a121e5d
Rahul Ghosh <154549822+rahulghosh111111@users.noreply.github.com>
2024-09-14 07:35:51 +0000

    string

--- 1_basics/string.js
+++ 1_basics/string.js
@@ -3,6 +3,41 @@ const github = "rahulghosh111111"
 
 // console.log(name + roll + " Ka github");
 
-console.log(`Hello my name is ${name} and my github id is ${github}`);
+// console.log(`Hello my name is ${name} & my github id is ${github}`);
 
-const game
+const gameName = new String("ClashOfClan")
+
+// console.log(gameName[6])
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());  // convert into upper case(Block letter)
+// console.log(gameName.toLowerCase());
+// console.log(gameName.charAt(6));
+
+// console.log(gameName.indexOf("l"));
+
+
+const newString = gameName.substring(0, 5)
+// console.log(newString);
+
+
+const anotherString = gameName.slice(-7, 5)
+// console.log(anotherString);  // 7th index to 5th index (5th index not included) from the end
+
+
+const newString1 = "   Rahul    "
+
+// console.log(newString1);
+// console.log(newString1.trim());   // remove white space from start and end
+
+const url = "https://github.com/rahul%20ghosh111111"
+
+// console.log(url.replace("%20", ""));
+// console.log(url.includes("rahul")); // return true or false
+
+// console.log(url.includes("hello"));
+
+
+// console.log(url.split("%20"));  // return array
+
+
+
