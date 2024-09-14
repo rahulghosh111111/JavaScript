bf03e3eed7851e6b98d697548abba29664f76353
Rahul Ghosh <154549822+rahulghosh111111@users.noreply.github.com>
2024-09-14 06:16:35 +0000

    string

---
+++ 1_basics/string.js
@@ -0,0 +1,8 @@
+const name = " Rahul"
+const github = "rahulghosh111111"
+
+// console.log(name + roll + " Ka github");
+
+console.log(`Hello my name is ${name} and my github id is ${github}`);
+
+const game
