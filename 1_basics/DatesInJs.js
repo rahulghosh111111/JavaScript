8a001e5abf0788e5ddf64ca1f2c725ad619affa7
Rahul Ghosh <rahulghosh111111@gmail.com>
2024-09-27 23:54:58 +0530

    Dates in javaScript

---
+++ 1_basics/DatesInJs.js
@@ -0,0 +1,42 @@
+let myDate = new Date()
+// console.log(myDate);
+
+// console.log(myDate +" ");
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toDateString());
+// console.log(typeof myDate);
+
+
+// let myCreatedDate = new Date(2020,0,3)
+// console.log(myCreatedDate.toDateString())
+
+
+// let myCreatedDate = new Date(2020,0,3,5,3,23,4)
+// console.log(myCreatedDate.toLocaleString())
+
+
+let myCreatedDate = new Date("2020-01-01")
+// console.log(myCreatedDate.toLocaleString())
+
+
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime())
+// console.log(Math.floor(Date.now()/1000));
+
+
+
+let newDate = new Date()
+// console.log(newDate.getDate());
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getFullYear());
+// console.log(newDate.getDay());
+// console.log(newDate.getHours());
+// console.log(newDate.getMinutes());
+// console.log(newDate.getSeconds());
+// console.log(newDate.getMilliseconds());
+// console.log(newDate.getTimezoneOffset());
+
+
+console.log(`${newDate.getDate() }-${newDate.getMonth() + 1}-${newDate.getFullYear()}`);
