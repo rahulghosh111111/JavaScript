ccffa97833c3b215bd9cb5871e2700d4360b6de1
Rahul Ghosh <rahulghosh111111@gmail.com>
2024-09-30 00:47:13 +0530

    ObjectsInJs

---
+++ 2_basics/Objects.js
@@ -0,0 +1,43 @@
+// Objects In JavaScript
+//Singleton---------> Object with only one property
+//Object.create
+
+
+
+
+// Object literals
+const mySym = Symbol("Rahul")   // symbol is unique  ***
+const JsUser = {
+    name: "Rahul",
+    age : 22,
+    location : "KIIT",
+    isMarried : false,
+    email: "rahul@123",
+    lastLoginDays : ["Mondays","Tuesdays"],
+    [mySym] : "myKey1"
+}
+
+// console.log(JsUser.name);    //object defined
+// console.log(JsUser["name"]); //object defined
+// console.log(JsUser[mySym]);    // myKey1        ***
+
+
+// JsUser.email = "rahul@1234"   //change the value of the email to rahul@1234
+// Object.freeze(JsUser)   //freeze the object if you want to edit the object
+// JsUser.email = "rahul@chatgpt.com"   //it will not work because it is frozen
+
+// console.log(JsUser);
+
+
+
+//***************************Adding function into the object****************************/
+
+
+JsUser.greeting = function(){
+    console.log("Hello Coders");  //string interpolation to access the name of the Object
+    }
+JsUser.greetingTwo = function(){
+console.log(`Hello ${this.name}`);  //string interpolation to access the name of the Object
+}
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
